fd6117a7240f2d33ede3002157fe915f5b67af41
Latrache Abdel <abdel_latrache@Latraches-MBP.fritz.box>
2021-11-14 12:20:13 +0100

    improvinfg markers

--- src/components/maps/maps.tsx
+++ src/components/maps/maps.tsx
@@ -1,7 +1,7 @@
 import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
 
 import Button from "../../components/Button";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React, { memo, MutableRefObject, useRef, useState } from "react";
 import { Marker, Circle } from "react-native-maps";
 
@@ -14,10 +14,10 @@ const Maps = () => {
   const mapRef = useRef() as MutableRefObject<MapView>;
   
   const [region, setRegion] = useState({
-    latitude: 51.5079145,
-    longitude: -0.0899163,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01
+    latitude: 52.42684737018143,
+    longitude: 10.789546598381433,
+    latitudeDelta: 0.1922,
+    longitudeDelta: 0.1421
   });
   const Wolfsburg = {
     latitude: 52.42684737018143,
@@ -25,14 +25,44 @@ const Maps = () => {
     latitudeDelta: 0.1922,
     longitudeDelta: 0.1421
   };
-  const Weyhouse = {
-    latitude: 52.42684737018143,
-    longitude: 10.789546598381433,
-    latitudeDelta: 0.1922,
-    longitudeDelta: 0.1421
-  };
-  const getElect = () => {
-    mapRef.current.animateToRegion(Weyhouse, 3 * 1000);
+  const Markers = [{
+    title: '1.21 Euro/KW',
+    coordinates: {
+      latitude: 52.42684737018143,
+      longitude: 10.789546598381433,
+      latitudeDelta: 0.1922,
+      longitudeDelta: 0.1421
+    },
+  },
+  {
+    title: '1.8 Euro/KW Super charger',
+    coordinates: {
+      latitude: 52.42684737018143,
+      longitude: 10.809546598381433,
+      latitudeDelta: 0.1922,
+      longitudeDelta: 0.1421
+    }  
+  },
+  {
+    title: '1.23 Euro/KW',
+    coordinates: {
+      latitude: 52.41684737018143,
+      longitude: 10.729546598381433,
+      latitudeDelta: 0.1922,
+      longitudeDelta: 0.1421
+    }  
+  },
+  {
+    title: '1.5 Euro/KW ',
+    coordinates: {
+      latitude: 52.40684737018143,
+      longitude: 10.859546598381433,
+      latitudeDelta: 0.1922,
+      longitudeDelta: 0.1421
+    },  
+  }]
+  const getElect = (location: any) => {
+    mapRef.current.animateToRegion(location, 3 * 1000);
   };
   const state = {
     LATLNG: {
@@ -60,22 +90,25 @@ const Maps = () => {
                 radius = { 5000 }
                 strokeWidth = { 2 }
                 strokeColor = { '#3399ff' }
-                fillColor = { 'rgba(230,238,255,0.6)' }
+                fillColor = { 'rgba(230,238,255,0.7)' }
                 zIndex = {-1}
         />
-        <Marker
-          coordinate={Wolfsburg}
-          pinColor="green"
-          image={require("../../assets/pin.png")}
-        />
-        <Marker
-          coordinate={Weyhouse}
-          pinColor="blue"
-          image={require("../../assets/pin.png")}
-        />
+        {Markers.map((marker) => {
+           return <Marker key={marker.title}
+           coordinate={marker.coordinates}
+           title={marker.title}
+           pinColor="green"
+           >
+              <Image
+               source={require("../../assets/pin.png")}
+               style={{width: 80, height: 86}}
+               resizeMode="contain"
+             />
+           </Marker>
+        })}
       </MapView>
-      <Button mode="outlined"onPress={() => getElect()} >
-      Find Electricity
+      <Button mode="outlined"onPress={() => getElect(Wolfsburg)} >
+      Go to Wolfsburg
       </Button>
     </>
   );
